d61683aef0336c174e03141f53a17a6b90e12caa
Kyle Ilantzis <ilantz.kyle@gmail.com>
2014-04-05 01:32:04 -0400

    fix jersey routing

--- src/main/java/simplejersey/App.java
+++ src/main/java/simplejersey/App.java
@@ -3,6 +3,7 @@ package simplejersey;
 import epic.templating.RenderingEngine;
 import epic.templating.jaxrs.ElementBodyWriter;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -47,21 +48,20 @@ public class App
         // This container (which is a servlet) will route requests to our resources
         ServletContainer sc = new ServletContainer( rc );
 
-        // To register a servlet instance with a ServletHandler we need to wrap the instance
-        // in a holder.
+        // To register a servlet instance we need to wrap it in a holder.
         ServletHolder h = new ServletHolder( sc );
 
-        // Jetty server needs a handler to process requests.
-        // We use a handler that forwards to servlets
-        ServletHandler sh = new ServletHandler();
-        // and register an jersey resource config (which is wrapped in a ServletContainer which is wrapped in
-        // a ServletHolder)
-        sh.addServletWithMapping( h, "/" );
+        // Jetty server needs some sort of handler to process requests.
+        ServletContextHandler sch = new ServletContextHandler();
+        // We will take the root path for this application
+        sch.setContextPath( "/" );
+        // All request that enter this context should go to jersey
+        sch.addServlet( h, "/*" );
 
         // The simplest possibly jetty server
         Server server = new Server(8080);
         // and we add our handler that forwards to jersey
-        server.setHandler( sh );
+        server.setHandler( sch );
         // run the simplest jetty server
         server.start();
         server.join();
